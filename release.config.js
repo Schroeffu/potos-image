91779997f2b5a7c9e739f2e4f51afc64a8d5d558
David Schroff <david.schroff@adfinis.com>
2022-11-15 12:54:44 +0100

    Revert "feat(semantic-releases): testing the semantic-releases feature"
    This reverts commit 565cb8cc8cd1f0ecea98c47201050c445f679ed4.

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    branches: "['+([0-9])?(.{+([0-9]),x}).x', 'master', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}]",
-    repositoryUrl: "https://github.com/projectpotos/potos-image",
-    plugins: [
-        "@semantic-release/commit-analyzer", 
-        "@semantic-release/release-notes-generator", 
-        "@semantic-release/github"
-    ]
-}
