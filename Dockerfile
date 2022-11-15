b5c204c4432202b49742f2de430b75062d1d89da
David Schroff <david.schroff@adfinis.com>
2022-11-16 00:20:02 +0100

    feat(firstboot_vars): Make Docker ARGs .env and ENV potos_env available at Potos first boot.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,31 @@
 FROM ubuntu:22.04
 
+# ARGs used during docker-compose build, from .env
+ARG POTOS_FULL_DISK_ENCRYPTION_INITIAL_PASSWORD
+ARG POTOS_INITIAL_HOSTNAME
+
+# ENVs used inside of the container at start docker-compose up, from potos_env file
+ENV POTOS_SPECS_REPOSITORY=$POTOS_SPECS_REPOSITORY
+ENV POTOS_ADJOIN=$POTOS_ADJOIN
+ENV POTOS_ENV=$POTOS_ENV
+
 WORKDIR /potos-iso
+
+# Install ISO creation depencies
 RUN apt update && apt install -y gfxboot p7zip-full xorriso wget curl libhtml-parser-perl cpio
+
 COPY potos-iso .
+
+# Copy all the ARGs and ENVs, to make them available the Potos ISO at boot.
+COPY potos_env contrib/scripts/potos_env
+COPY .env contrib/scripts/.env
+
+# Overwrite LUKS Install Key according .env
+RUN sed -i "s/key: .*/key: $POTOS_FULL_DISK_ENCRYPTION_INITIAL_PASSWORD/g" "autoinstall/desktop-bios/user-data"
+RUN sed -i "s/key: .*/key: $POTOS_FULL_DISK_ENCRYPTION_INITIAL_PASSWORD/g" "autoinstall/desktop-uefi/user-data"
+
+# Overwrite Initial Hostname, according .env
+RUN sed -i "s/hostname: .*/hostname: $POTOS_INITIAL_HOSTNAME/g" "autoinstall/desktop-bios/user-data"
+RUN sed -i "s/hostname: .*/hostname: $POTOS_INITIAL_HOSTNAME/g" "autoinstall/desktop-uefi/user-data"
 
 CMD ["./create-iso", "-p"]
---
+++ potos-iso/contrib/scripts/firstboot-gui.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Load all the environment variables
+source /setup/potos_env
+source /setup/.env
+
+POTOS_VERSION="Potos Linux Client"
+
+if [[ -f /setup/potos-version ]]; then
+  POTOS_VERSION="$(</setup/potos-version)"
+fi
+
+yad --fullscreen --title 'Potos Setup' \
+  --borders 20 --align center --button OK --image-on-top \
+  --image=/potos-setup/potos.png \
+  --text \
+"Welcome to the last step of the Potos installation
+
+Potos: ${POTOS_VERSION}
+
+Environment Variables:
+POTOS_SPECS_REPOSITORY: ${POTOS_SPECS_REPOSITORY}
+POTOS_ADJOIN: ${POTOS_ADJOIN}
+POTOS_ENV: ${POTOS_ENV}
+POTOS_FULL_DISK_ENCRYPTION_INITIAL_PASSWORD: ${POTOS_FULL_DISK_ENCRYPTION_INITIAL_PASSWORD}
+POTOS_INITIAL_HOSTNAME: ${POTOS_INITIAL_HOSTNAME}
+"
+
+#sudo systemctl restart gdm.service
+
+################################################################################
+# Finalize the installation
+################################################################################
+
+sed -ie 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/' /etc/default/grub
+/usr/sbin/update-grub
+
+cat > /var/lib/AccountsService/users/admin <<EOF
+[User]
+SystemAccount=true
+EOF
+
+echo "# Cleaning up ... please wait"
+
+rm -f /etc/sudoers.d/01_gnome-initial-setup
+apt purge -y gnome-initial-setup
+userdel -rf gnome-initial-setup
+
+rm -rf /setup
+
+sleep 5s
+systemctl reboot
+
+# vim: tabstop=2 shiftwidth=2 expandtab softtabstop=2
+
