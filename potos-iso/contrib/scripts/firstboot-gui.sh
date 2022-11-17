8016ca6c80a5e300a32b07c5930924b76bd2139d
David Schroff <david.schroff@adfinis.com>
2022-11-17 10:33:03 +0100

    feat(vars): git clone and run the specific specs repo according .env vars

--- potos-iso/contrib/scripts/firstboot-gui.sh
+++ potos-iso/contrib/scripts/firstboot-gui.sh
@@ -27,32 +27,85 @@ POTOS_FULL_DISK_ENCRYPTION_INITIAL_PASSWORD: ${POTOS_FULL_DISK_ENCRYPTION_INITIA
 POTOS_INITIAL_HOSTNAME: ${POTOS_INITIAL_HOSTNAME}
 POTOS_CLIENT_NAME: ${POTOS_CLIENT_NAME}
 POTOS_CLIENT_SHORTNAME: ${POTOS_CLIENT_SHORTNAME}
+
+POTOS_GIT_SPECS_URL: ${POTOS_GIT_SPECS_URL}
+POTOS_GIT_SPECS_REPO: ${POTOS_GIT_SPECS_REPO}
+POTOS_GIT_SPECS_BRANCH: ${POTOS_GIT_SPECS_BRANCH}
+
+
+Click OK to proceed the installation.
 "
 
-#sudo systemctl restart gdm.service
+while [[ -z ${POTOS_USER} || -z ${POTOS_PASS} ]]; do
+  USERINPUT="$(yad --fullscreen --title '${POTOS_CLIENT_NAME} Setup' \
+    --borders 20 --align center \
+    --button gtk-ok --button "Change keyboard layout":"/setup/change-keyboard-layout" \
+    --image-on-top --image=/potos-setup/potos.png \
+    --text \
+"IMPORTANT: If your password contains special characters, please remember to select the appropriate keyboard layout first.
 
-################################################################################
-# Finalize the installation
-################################################################################
+Please enter your new credentials below 
+" \
+    --form \
+    --field 'Username' \
+    --field 'Password:H'
+  )"
 
-sed -ie 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/' /etc/default/grub
-/usr/sbin/update-grub
+  POTOS_USER="$(echo ${USERINPUT} | cut -d '|' -f 1)"
+  POTOS_PASS="$(echo ${USERINPUT} | cut -d '|' -f 2)"
 
-cat > /var/lib/AccountsService/users/admin <<EOF
-[User]
-SystemAccount=true
-EOF
+  if [[ -z ${POTOS_USER} || -z ${POTOS_PASS} ]]; then
+    yad --title '${POTOS_CLIENT_NAME} Setup' \
+      --borders 20 --align center --button gtk-ok --image-on-top \
+      --image=/potos-setup/potos.png \
+      --text 'Please choose and enter your new credentials.'
+  fi
+done
 
-echo "# Cleaning up ... please wait"
+export POTOS_USER POTOS_PASS
 
-rm -f /etc/sudoers.d/01_gnome-initial-setup
-apt purge -y gnome-initial-setup
-userdel -rf gnome-initial-setup
+sudo mkdir -m 755 -p /var/log/${POTOS_CLIENT_SHORTNAME}
+sudo touch /var/log/${POTOS_CLIENT_SHORTNAME}/setup.log
+sudo -E bash -c '/setup/finish.sh &> /var/log/${POTOS_CLIENT_SHORTNAME}/setup.log' &
 
-rm -rf /setup
+FINISH_CMD_PID=${!}
 
-sleep 5s
-systemctl reboot
+sudo chown gnome-initial-setup /dev/tty2
 
-# vim: tabstop=2 shiftwidth=2 expandtab softtabstop=2
+sudo tail -f /var/log/${POTOS_CLIENT_SHORTNAME}/setup.log | tee /dev/tty2 | yad --fullscreen --no-buttons --title '${POTOS_CLIENT_NAME} Setup' \
+  --progress --enable-log --log-expanded --log-on-top --log-height 500 \
+  --text 'Please wait until the setup is finished' &
 
+# Wait until the finish.sh background process exits and check its return code
+wait ${FINISH_CMD_PID}
+
+FINISH_CMD_RC=${?}
+
+if [[ ${FINISH_CMD_RC} -ne 0 ]]; then
+  case ${FINISH_CMD_RC} in
+    1) ERROR_MSG="General command failure" ;;
+    2) ERROR_MSG="Network connectivity issues" ;;
+    3) ERROR_MSG="AD join failure" ;;
+    4) ERROR_MSG="Ansible failure" ;;
+    5) ERROR_MSG="Vault failure" ;;
+    6) ERROR_MSG="ISO outdated" ;;
+    *) ERROR_MSG="Unknown error code ${FINISH_CMD_RC}" ;;
+  esac
+fi
+
+sudo cat /var/log/${POTOS_CLIENT_SHORTNAME}/setup.log | yad --fullscreen --title '${POTOS_CLIENT_NAME} Setup' \
+  --borders 20 --align center --button gtk-ok \
+  --button "Shutdown":"sudo systemctl halt" \
+  --text-info --tail \
+  --image=/setup/potos.png --image-on-top \
+  --text \
+"Installation failed
+
+The last step of the installation was not successful.
+  Error message: ${ERROR_MSG}
+  Return code: ${FINISH_CMD_RC}
+
+Click OK to retry or Shutdown to cancel.
+"
+
+sudo systemctl restart gdm.service
